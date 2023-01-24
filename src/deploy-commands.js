d5c94c4a68a2c8208e31508ac19fe77904b200af
Emile Villette <emile.villette@student.uclouvain.be>
2023-01-25 00:34:52 +0100

    update REST version

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,32 +1,32 @@
 const fs = require('fs')
-const { REST } = require('@discordjs/rest')
-const { Routes } = require('discord-api-types/v9')
+const {REST} = require('@discordjs/rest')
+const {Routes} = require('discord-api-types/v10')
 const dotenv = require('dotenv')
 dotenv.config()
 
 const commands = []
 const commandFiles = fs
-  .readdirSync('./commands')
-  .filter(file => file.endsWith('.js'))
+    .readdirSync('./commands')
+    .filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-  if (file.startsWith('.')) {
-    continue;
-  }
+    if (file.startsWith('.')) {
+        continue;
+    }
 
-  const command = require(`./commands/${file}`)
-  commands.push(command.data.toJSON())
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
 }
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
+const rest = new REST({version: '10'}).setToken(process.env.DISCORD_TOKEN)
 
 rest
-  .put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID,
-    ),
-    { body: commands },
-  )
-  .then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error)
+    .put(
+        Routes.applicationGuildCommands(
+            process.env.CLIENT_ID,
+            process.env.GUILD_ID,
+        ),
+        {body: commands},
+    )
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error)
