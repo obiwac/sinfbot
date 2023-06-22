c6684ba77ae4a993e3e32e6b4a41f4bf89c64220
Emile Villette <emile.villette@student.uclouvain.be>
2023-06-22 17:25:18 +0200

    directory creation + draft of meme contest log file
    Signed-off-by: Emile Villette <emile.villette@student.uclouvain.be>

--- src/commands/creatememecontest.js
+++ src/commands/creatememecontest.js
@@ -1,16 +1,36 @@
 const {SlashCommandBuilder} = require("@discordjs/builders")
 const fs = require('fs');
 const {check_interaction_privilege} = require("../utils/require_privilege");
+const {ChannelType} = require("discord-api-types/v9");
 require("dotenv").config({path: "../.env"})
+
+const MEME_CONTEST_DIRECTORY = "../meme_constest/"
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('creatememecontest')
-        .setDescription('Meme contest creator'),
+        .setDescription('Meme contest creator')
+        .addChannelOption(option => option
+            .setName("contestchannel")
+            .setDescription("Channel Where the contest will take place")
+            .setRequired(true)
+            .addChannelTypes(ChannelType.GuildText))
+        .addStringOption(option => option
+            .setName("emoji")
+            .setDescription("Custom emoji to use for reaction counting")
+            .setRequired(true)),
 
     async execute(client, interaction) {
         check_interaction_privilege(interaction);
 
-        return interaction.reply({content: "Meme contest created (NOT FUNCTIONAL YET)", ephemeral: true})
+        const contestChannel = interaction.options.getChannel("contestchannel");
+        const contestEmoji = interaction.options.getString("emoji");
+
+        if (!fs.existsSync(MEME_CONTEST_DIRECTORY)) {
+            fs.mkdirSync(MEME_CONTEST_DIRECTORY);
+        }
+
+
+        return interaction.reply({content: "Meme contest created (NOT FUNCTIONAL YET)", ephemeral: true});
     },
 }
